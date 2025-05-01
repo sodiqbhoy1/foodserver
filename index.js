910959251952a12394438ed3502c68c66d3a36dd
sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-05-01 18:35:11 +0100

    fix: remove trailing slash from CORS origin URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://foodexpress-neon.vercel.app/', // or your frontend URL
+  origin: 'https://foodexpress-neon.vercel.app', // or your frontend URL
   credentials: true
 }));
 
